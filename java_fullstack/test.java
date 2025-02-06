5f18db2d563e68b005464af29094cb0d8a2b6404
Mayank Gaur <mayankgaur1504@gmail.com>
2025-02-06 20:47:41 +0530

    testing done

---
+++ java_fullstack/test.java
@@ -0,0 +1,9 @@
+public class test {
+ public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int num1 = 10;
+        int num2 = 2;
+        int rem = num1%num2;
+        System.out.print("Remainder of num1/num2 is " +rem);
+ }
+}
